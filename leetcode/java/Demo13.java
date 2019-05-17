fcd079507d12045b2b85909a48472226cbf6edec
guijianshi <15280675745@163.com>
2019-05-17 09:17:55 +0800

    13题整数转罗马数字

---
+++ leetcode/java/Demo13.java
@@ -0,0 +1,22 @@
+public class Demo13
+{
+    public static void main(String[] args)
+    {
+        System.out.println(intToRoman(3999));
+        System.out.println(intToRoman(288));
+    }
+
+    private static String intToRoman(int num)
+    {
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] reps = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                result.append(reps[i]);
+            }
+        }
+        return result.toString();
+    }
+}
