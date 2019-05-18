9bd49a7abb1137214b87159efba58b4a2b197f15
guijianshi <15280675745@163.com>
2019-05-18 13:06:38 +0800

    第15题

--- leetcode/java/Demo14.java
+++ leetcode/java/Demo14.java
@@ -2,19 +2,20 @@ public class Demo14
 {
     public static void main(String[] args)
     {
-        String[] strs = {"flower","flow","flight"};
+        String[] strs = {"f","f","f"};
         System.out.println(longestCommonPrefix(strs));
     }
 
     /**
-     * @// TODO: 2019/5/17  
-     * @param strs
+     * @param strs 字符数组
      * @return
      */
     private static String longestCommonPrefix(String[] strs)
     {
         if (0 == strs.length) {
             return "";
+        } else if (1 == strs.length) {
+            return strs[0];
         }
         String s = "";
         int minLength = strs[0].length();
@@ -22,7 +23,7 @@ public class Demo14
             minLength = Math.min(minLength, str.length());
         }
         int commonPrefixLength = 0;
-        outterLoop:for (int i = 1; i < minLength; i++) {
+        outterLoop:for (int i = 1; i < minLength + 1; i++) {
             s = strs[0].substring(0, i);
             for (String str : strs) {
                 if (!str.startsWith(s)) {
